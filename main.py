2858f83c81bf14bcffdc83f7694e4abfd41d4e8e
Arne Munthe-Kaas <arnegiacomo@gmail.com>
2023-03-11 15:18:24 +0100

    print results, add spash screen and info table

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import pyperclip
 import uncurl
 import threading
 import queue
+from tabulate import tabulate
+from pyfiglet import Figlet
 
 DEFAULT_REQUESTS = 10
 DEFAULT_THREADS = 1
@@ -30,8 +32,10 @@ def benchmark_worker(context, requests, refreshtime):
         payload = json.loads(data)
 
     response_times = []
+    successes = 0
 
     for i in range(requests):
+
         try:
             start_time = time.time()
             response = r.request(
@@ -45,6 +49,9 @@ def benchmark_worker(context, requests, refreshtime):
             delta_time = end_time - start_time
             response_times.append(delta_time)
 
+            if response.status_code == 200:
+                successes += 1
+
             print(f"[{threading.current_thread().name}] Sent {method} request to {url}")
             print(f"[{threading.current_thread().name}] Response status code: {response.status_code}")
             print(f"[{threading.current_thread().name}] Response time: {delta_time:.2f} seconds")
@@ -55,17 +62,52 @@ def benchmark_worker(context, requests, refreshtime):
 
         time.sleep(refreshtime)
 
-    if len(response_times) != 0:
-        average_response_time = sum(response_times) / len(response_times)
-        median_response_time = statistics.median(response_times)
-        timestamp = datetime.now()
+    average_response_time = sum(response_times) / len(response_times)
+    median_response_time = statistics.median(response_times)
+    success_percentage = successes / requests * 100
 
-        result_queue.put(average_response_time)
-    else:
-        print(f"[{threading.current_thread().name}] No successful {method} requests to {url}")
+    result = {
+        "Average response time": average_response_time,
+        "Median response time": median_response_time,
+        "Fastest response": min(response_times),
+        "Longest response": max(response_times),
+        "Successful responses": successes,
+        "Success rate": success_percentage,
+        "Thread": threading.current_thread().name
+    }
+
+    result_queue.put(result)
+
+    print(f"[{threading.current_thread().name}] has finished!")
+
+
+def print_results():
+    print()
+    result_list = list(result_queue.queue)
+    table = tabulate(result_list, headers="keys", tablefmt="grid")
+    print(table)
+
+
+def print_run_info(context, requests, threads, thread_creation_delay, appname, comment, refreshtime):
+    # Truncate the URL if it's longer than MAX_URL_WIDTH characters
+    truncated_url = context.url[:40] + "..." if len(context.url) > 40 else context.url
+
+    table = [
+        ["URL:", truncated_url],
+        ["Request amount:", requests],
+        ["Threads:", threads],
+        ["Thread creation delay:", thread_creation_delay],
+        ["App name:", appname],
+        ["Comments:", comment],
+        ["Refresh time:", refreshtime],
+    ]
+
+    print(tabulate(table, colalign=["center", "left"], tablefmt="grid"))
+    print("")
 
 
 def start_threads(context, requests, refreshtime, number_of_threads, thread_creation_delay):
+    before_requests = datetime.now()
 
     for i in range(number_of_threads):
         thread = threading.Thread(target=benchmark_worker, args=(context, requests, refreshtime),
@@ -74,20 +116,32 @@ def start_threads(context, requests, refreshtime, number_of_threads, thread_crea
         worker_threads.append(thread)
         time.sleep(thread_creation_delay)
 
+    for thread in worker_threads:
+        thread.join()
+
+    after_requests = datetime.now()
+    benchmark_time = after_requests - before_requests
+
+    print_results()
+
 
 def main(
         file: Optional[typer.FileText] = typer.Argument(None,
                                                         help='Path to file containing a cURL command to run. Will use clipboard value if not provided. \033[1mMust only contain a single valid cURL command!\033[0m'),
-        requests: int = typer.Option(DEFAULT_REQUESTS, "--requests", "-r", help='Number of times to run cURL command.', min=1),
+        requests: int = typer.Option(DEFAULT_REQUESTS, "--requests", "-r", help='Number of times to run cURL command.',
+                                     min=1),
         threads: int = typer.Option(DEFAULT_THREADS, "--threads", "-t",
                                     help='Number of threads to run cURL commands in parallel.', min=1),
         thread_creation_delay: int = typer.Option(DEFAULT_THREAD_CREATION_DELAY, "--delay", "-d",
-                                    help='Delay between creation of worker threads.', min=0),
+                                                  help='Delay between creation of worker threads.', min=0),
         refreshtime: int = typer.Option(DEFAULT_REFRESHTIME, "--refreshtime",
                                         help='Number of seconds between cURL commands.', min=0),
         appname: str = typer.Option(None, "--name", "-n", help='Name of application.'),
         comment: str = typer.Option(None, "--comment", "-c", help='Optional comments.'),
 ):
+    f = Figlet(font='slant')
+    print(f.renderText('HttPyBench'))
+
     if file is None:
         # If not provided with file, get clipboard value
         curl = pyperclip.paste().encode().decode('unicode_escape')
@@ -97,17 +151,7 @@ def main(
 
     context = uncurl.parse_context(curl)
 
-    print("=" * 40)
-    print(f"{'HttPyBench':^40}")
-    print("=" * 40)
-    print(f"{'URL:':<25}{context.url}")
-    print(f"{'Request amount:':<25}{requests}")
-    print(f"{'Threads:':<25}{threads}")
-    print(f"{'Thread creation delay:':<25}{thread_creation_delay}")
-    print(f"{'App name:':<25}{appname}")
-    print(f"{'Comments':<25}{comment}")
-    print(f"{'Refresh time:':<25}{refreshtime}")
-    print("=" * 40)
+    print_run_info(context, requests, threads, thread_creation_delay, appname, comment, refreshtime)
 
     start_threads(context, requests, refreshtime, threads, thread_creation_delay)
 
@@ -120,7 +164,6 @@ def main(
 
 if __name__ == "__main__":
     typer.run(main)
-
 
 # info = {
 #     "appName": appName,
